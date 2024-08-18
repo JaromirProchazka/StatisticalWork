7f7ec627eae2888c397c30e942c47ff1b28db530
Jaromir Prochazka <jaromir.prochazka03@gmail.com>
2024-08-18 20:11:18 +0200

    Add README and program results

--- Program.py
+++ Program.py
@@ -23,24 +23,28 @@ def main() -> None:
 
     stat.reformat_cache(line_count_power=11, block_size_power=5, associativity_power=0).experiment()
     stat.reformat_cache(line_count_power=12, block_size_power=4, associativity_power=0).experiment()
+    stat.reformat_cache(line_count_power=14, block_size_power=2, associativity_power=0).experiment()
     print('Raising Line count, falling Block size - DONE')
 
     stat.reformat_cache(line_count_power=9, block_size_power=7, associativity_power=0).experiment()
     stat.reformat_cache(line_count_power=8, block_size_power=8, associativity_power=0).experiment()
+    stat.reformat_cache(line_count_power=6, block_size_power=10, associativity_power=0).experiment()
     print('Raising Block size, falling Line count - DONE')
 
     stat.reformat_cache(line_count_power=10, block_size_power=5, associativity_power=1).experiment()
     stat.reformat_cache(line_count_power=10, block_size_power=4, associativity_power=2).experiment()
+    stat.reformat_cache(line_count_power=9, block_size_power=4, associativity_power=3).experiment()
     print('Raising Associativity, falling Block size - DONE')
 
     stat.reformat_cache(line_count_power=9, block_size_power=6, associativity_power=1).experiment()
     stat.reformat_cache(line_count_power=8, block_size_power=6, associativity_power=2).experiment()
+    stat.reformat_cache(line_count_power=6, block_size_power=6, associativity_power=4).experiment()
     print('Raising Associativity, falling Line count - DONE')
 
-    stat.reformat_cache(line_count_power=6, block_size_power=6, associativity_power=4).experiment()
+    stat.reformat_cache(line_count_power=5, block_size_power=6, associativity_power=5).experiment()
     print('Large Associativity against Line count - DONE')
-    # stat.reformat_cache(line_count_power=10, block_size_power=4, associativity_power=4).experiment()
-    # print('Fully associative - DONE')
+    # sstat.reformat_cache(line_count_power=10, block_size_power=4, associativity_power=4).experiment()
+    # pprint('Fully associative - DONE')
 
     print()
     stat.log_results()
--- StatisticalAnalysis/logger.py
+++ StatisticalAnalysis/logger.py
@@ -5,12 +5,13 @@ from typing import Self
 class _StatLogger:
     def __init__(self) -> None:
         self._rows: list = list()
-        self._add_header()
     
-    def _add_header(self):
-        self._rows.append(['Line Count', 'Block size [B]', 'Associativity', 'Lambda'])
+    def _get_header(self) -> list:
+        return ['Line Count', 'Block size [B]', 'Associativity', 'Lambda']
     
     def log(self) -> None:
+        self._rows.sort(key= lambda x: x[-1])
+        self._rows.insert(0, self._get_header())
         print(tabulate(self._rows))
     
     def add_row(self, line_cont: int, bloc_size: int, associativity: int, lambda_res: float) -> Self:
@@ -19,5 +20,4 @@ class _StatLogger:
     
     def clear_rows(self) -> Self:
         self._rows.clear()
-        self._add_header()
         return self
